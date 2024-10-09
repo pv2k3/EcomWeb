b3975cf9b5ec88ae0d13279cf4471100c25bc197
pv2k3 <vermapriyanshu001@gmail.com>
2024-10-10 00:52:10 +0530

    remade the admin page completely

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const { getUser } = require("../services/auth");
+const User = require("../models/USER");
 
 const router = express.Router();
 
-router.get("/", (req, res)=>{
-    res.render("admin")
+router.get("/", async (req, res)=>{
+    const userUid = req.cookies.uid;
+
+    const user = getUser(userUid);
+    const name = await User.findById(user.id, {userName: 1, _id:0});
+
+    res.render("admin", {
+        name: name,
+    })
 })
 .get("/addItem", (req, res) => {
     res.render("addStock")
